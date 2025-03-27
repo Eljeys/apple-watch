526d4c1ccad98e79ce63771e4fe1fac9d779db8d
eljeys <eljeys1@gmail.com>
2025-03-27 13:41:05 +0100

    icons import

---
+++ src/app/components/subcomponent/icons/Icons.jsx
@@ -0,0 +1,12 @@
+"use client";
+import { FaApple } from "react-icons/fa6";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { LuShoppingBag } from "react-icons/lu";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+const icons = () => {
+  return;
+  <div></div>;
+};
+
+export default icons;
