f926a62636f59a20e670f204b1d62080d9374579
eljeys <eljeys1@gmail.com>
2025-03-27 17:07:26 +0100

    info-card done

--- src/app/components/maincomponents/Info-card.jsx
+++ src/app/components/maincomponents/Info-card.jsx
@@ -5,9 +5,11 @@ import { FaLongArrowAltLeft } from "react-icons/fa";
 const InfoCard = () => {
   return (
     <section className="info-card-container">
-      <h1>
-        The Perfect Moment <br></br> Between Past And <br></br> Future.
-      </h1>
+      <div className="info-card-heading">
+        <h1>
+          The Perfect Moment <br></br> Between Past And <br></br> Future.
+        </h1>
+      </div>
       <BuyButton />
       <div className="arrow-container">
         <FaLongArrowAltLeft />
