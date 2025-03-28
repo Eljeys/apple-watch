9f77c785ac2d9d6bef303ff05dadc3a7435bcbb6
eljeys <eljeys1@gmail.com>
2025-03-28 10:32:02 +0100

    Product-card-select-buttons_V1

---
+++ src/app/components/subcomponent/buttons/Select-button.jsx
@@ -0,0 +1,8 @@
+"use client";
+import { FaRegCircle } from "react-icons/fa";
+
+const SelectButton = () => {
+  return <FaRegCircle size={24} />;
+};
+
+export default SelectButton;
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Header from "./components/maincomponents/Header";
 import InfoCard from "./components/maincomponents/Info-card";
 import ProductCard from "./components/maincomponents/Product-card";
+import SelectButton from "./components/subcomponent/buttons/Select-button";
 export default function Home() {
   return (
     <main>
@@ -10,8 +11,13 @@ export default function Home() {
         <div className="info-card-wrapper">
           <InfoCard />
         </div>
-        <div className="product-card-wrapper">
+        <section className="product-card-wrapper">
           <ProductCard />
+        </section>
+        <div className="selec-button-wrapper">
+          <SelectButton />
+          <SelectButton />
+          <SelectButton />
         </div>
       </section>
     </main>
