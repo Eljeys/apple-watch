12d264c087d69ed33b1f32a7b4a10e4a8036310a
eljeys <eljeys1@gmail.com>
2025-04-02 10:51:15 +0200

    Header refactor tailwind CSS

--- src/app/components/maincomponents/Header.jsx
+++ src/app/components/maincomponents/Header.jsx
@@ -7,34 +7,34 @@ import { LuShoppingBag } from "react-icons/lu";
 
 const Header = () => {
   return (
-    <header>
+    <header className="flex items-center justify-between py-4 mb-20">
       <div className="logo">
         <FaApple size={45} />
       </div>
       <nav>
-        <ul className="nav-list">
-          <li>
+        <ul className="flex flex-row gap-8 items-center">
+          <li className="hover:font-bold hover:rounded-[0.5rem] hover:py-[0.5rem] hover:px-4 hover:w-fit hover:bg-white">
             <NavLink linkName={"Mac"} />
           </li>
-          <li>
+          <li className="hover:font-bold hover:rounded-[0.5rem] hover:py-[0.5rem] hover:px-4 hover:w-fit hover:bg-white">
             {" "}
             <NavLink linkName={"iPhone"} />
           </li>
-          <li>
+          <li className="hover:font-bold hover:rounded-[0.5rem] hover:py-[0.5rem] hover:px-4 hover:w-fit hover:bg-white">
             {" "}
             <NavLink linkName={"iPad"} />
           </li>
-          <li>
+          <li className="hover:font-bold hover:rounded-[0.5rem] hover:py-[0.5rem] hover:px-4 hover:w-fit hover:bg-white">
             {" "}
             <NavLink linkName={"iWatch"} />
           </li>
-          <li>
+          <li className="hover:font-bold hover:rounded-[0.5rem] hover:py-[0.5rem] hover:px-4 hover:w-fit hover:bg-white">
             {" "}
             <NavLink linkName={"Support"} />
           </li>
         </ul>
       </nav>
-      <div className="icon-container">
+      <div className="flex gap-[0.5rem] w-10 mx-4">
         <FaMagnifyingGlass />
         <LuShoppingBag />
       </div>
